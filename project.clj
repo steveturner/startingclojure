c0fc05d2644150bda9ba33731d8b424b6fda4a05
Steven Turner <whathasnotbeenused@gmail.com>
2013-03-03 12:10:11 -0700

    Pallet config for java and datomic

--- project.clj
+++ project.clj
@@ -5,36 +5,48 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
-  :dependencies [[org.clojure/clojure "1.5.0"] 
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 
                  [ring "1.1.8"]
                  [compojure "1.1.5"]
                  [enlive "1.1.1"]
                  [com.datomic/datomic-free "0.8.3826"]
-                 [org.cloudhoist/pallet "0.7.3"]
-                 [org.cloudhoist/pallet-jclouds "1.4.3"]
-                 ;; To get started we include all jclouds compute providers.
-                 ;; You may wish to replace this with the specific jclouds
-                 ;; providers you use, to reduce dependency sizes.
-                 [org.jclouds/jclouds-allblobstore "1.4.2"]
-                 [org.jclouds/jclouds-allcompute "1.4.2"]
-                 [org.jclouds.driver/jclouds-slf4j "1.4.2"
-                  ;; the declared version is old and can overrule the
-                  ;; resolved version
-                  :exclusions [org.slf4j/slf4j-api]]
-                 [org.jclouds.driver/jclouds-sshj "1.4.2"]
-                 [ch.qos.logback/logback-classic "1.0.0"]]
-   :dev-dependencies [[org.cloudhoist/pallet
-                      "0.7.3" :type "test-jar"]
-                     [org.cloudhoist/pallet-lein "0.5.2"]]
-  :profiles {:dev
-             {:dependencies
-              [[org.cloudhoist/pallet "0.7.3" :classifier "tests"]]
-              :plugins [[org.cloudhoist/pallet-lein "0.5.2"]]}
-             :leiningen/reply
-             {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.2"]]
-              :exclusions [commons-logging]}}
+                                  ;; The latest pallet
+                 [com.palletops/pallet "0.8.0-beta.1"]
+
+                 ;; The latest jclouds fun
+                 [org.cloudhoist/pallet-jclouds "1.5.2"]
+                 [org.jclouds/jclouds-allblobstore "1.5.5"]
+
+                 [org.jclouds/jclouds-allcompute "1.5.5"]
+                 [org.jclouds.driver/jclouds-slf4j "1.5.5"]
+                 [org.jclouds.driver/jclouds-jsch "1.5.5"]
+                 [org.jclouds/jclouds-compute "1.5.5"]
+                 [org.jclouds/jclouds-blobstore "1.5.5"]
+                 [org.slf4j/slf4j-api "1.6.1"]
+                 [ch.qos.logback/logback-core "1.0.0"]
+                 [ch.qos.logback/logback-classic "1.0.0"]
+
+                 ;; Rackspace London and Greenqloud jclouds providers,
+                 ;; becuase if you are going to be in the cloud you
+                 ;; might as well be green. See
+                 ;; https://www.mastodonc.com/dashboard for more
+                 ;; information.
+                 [ com.palletops/java-crate "0.8.0-beta.1"]
+                 [datomic-crate "0.8.0-SNAPSHOT"]
+                 [org.jclouds.labs/greenqloud-compute "1.5.5"]
+                 ]
+ ; :exclusions [org.jclouds/jclouds-allblobstore
+  ;             org.jclouds/jclouds-allcompute
+   ;            org.jclouds/jclouds-compute
+    ;           org.jclouds/jclouds-blobstore
+     ;          org.jclouds.driver/jclouds-slf4j
+      ;         org.jclouds.driver/jclouds-jsch
+       ;        org.jclouds.labs/greenqloud-compute]
+  :dev-dependencies [[org.cloudhoist/pallet "0.7.1" :type "test-jar"]
+                     [org.cloudhoist/pallet-lein "0.5.1"]]
+  :profiles {:dev {:plugins [[org.cloudhoist/pallet-lein "0.5.1"]]}}
   :local-repo-classpath true
   :repositories
   {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
    "sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
-  
--- src/clojure/startingclojure/pallet.clj
+++ src/clojure/startingclojure/pallet.clj
@@ -1,2 +1,36 @@
-(ns startingclojure.pallet)
+(ns startingclojure.pallet
+  (:require
+    [pallet.actions :as actions]
+     [pallet.api :as api]
+     [pallet.configure :as configure]
+     [pallet.crate.automated-admin-user :as admin-user]
+     [pallet.phase :as phase]
+     
+     )
+  (:use pallet.crate.java pallet.crate.datomic))
 
+(api/converge
+  (api/group-spec "mygroup"
+   :extends [(pallet.crate.java/java {}) (pallet.crate.datomic/datomic {})]
+   :count 0
+   :node-spec (api/node-spec
+               :image {:os-family :ubuntu :image-id "us-east-1/ami-3c994355"}
+               )
+   :phases {:bootstrap ( api/plan-fn ( admin-user/automated-admin-user))
+            ;:configure (api/plan-fn (package "curl"))
+            })
+  :compute (configure/compute-service :aws))
+
+(api/converge
+   (api/group-spec "mygroup"
+                   :count 0
+                   :node-spec (api/node-spec
+                               :image {:os-family :ubuntu
+                                       :os-64-bit true}
+                               :hardware {:hardware-id "m1.large"}
+                               ;; Sao Paulo is the greenest AWS Location
+                               :location {:location-id "sa-east-1b"}
+                               ;; Oregon is the greenest cheap location (same price as VA)
+                               ;;:location {:location-id "us-west-2b"}
+                               :qos {:spot-price (float 0.50)}))
+   :compute (configure/compute-service :aws))
